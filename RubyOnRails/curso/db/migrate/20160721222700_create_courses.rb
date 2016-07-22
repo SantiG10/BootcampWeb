69fb07f9a8847efcd5eae62f165f806190d516f6
Santiago Giraldo <santi_rm8@hotmail.com>
2016-07-22 14:29:56 -0500

    Ejercicio sobre estudiantes en unos cursos

---
+++ RubyOnRails/curso/db/migrate/20160721222700_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.integer :credits
+      t.integer :duration
+
+      t.timestamps null: false
+    end
+  end
+end
